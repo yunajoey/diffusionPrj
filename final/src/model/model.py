63a059d39b55349fbd336d5b3b232ca093dd2d59
yuna <yunajoe@gmail.com>
2023-02-19 23:12:36 +0900

    model.py수정

--- final/src/model/model.py
+++ final/src/model/model.py
@@ -2,7 +2,6 @@
 # 나중에 argparse로 명령어로 model Class를 쳤을 때 그 클래스에 해당되는 model_name이 나오게 하기 
 # 출처: https://github.com/Inju0716/ko-en-neural-machine-translation/blob/main/Huggingface_Pretrained_Model.ipynb
 
-
 import argparse 
 import logging  
 import numpy as np
@@ -15,23 +14,20 @@ from transformers import (
 
 MODEL_CLASSES = {  
     "Auto" : (AutoTokenizer,  AutoModelForSeq2SeqLM)  
-}  
+}    
 
-
-MODEL_NAMES_LIST = {    
-      
+MODEL_NAMES_LIST = {         
        "QuoA": "QuoQA-NLP/KE-T5-Ko2En-Base"
+       
 }   
       
 model_class, tokenizer_class = MODEL_CLASSES['Auto']   
 
 
-def model_print(model_name):   
+def model_print(model_name):     
     tokenizer = model_class.from_pretrained(MODEL_NAMES_LIST[model_name])  
     model = tokenizer_class.from_pretrained(MODEL_NAMES_LIST[model_name])
     return tokenizer, model
-
-
 
 
 
